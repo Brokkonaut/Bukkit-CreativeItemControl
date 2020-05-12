408acd3eccd1690c90f33842559a5c2c339598df
Orlaando <38089755+Orlaando-dev@users.noreply.github.com>
2020-05-12 10:32:04 -0600

    Support for PlayerInteractEvent

--- src/main/java/org/cyberiantiger/minecraft/itemcontrol/Main.java
+++ src/main/java/org/cyberiantiger/minecraft/itemcontrol/Main.java
@@ -47,6 +47,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryCreativeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -311,6 +312,19 @@ public class Main extends JavaPlugin implements Listener {
         if (!checkBlacklist(p, itemTag)) {
             e.setCancelled(true);
             item.remove();
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        if (p.hasPermission(PERMISSION_BLACKLIST_BYPASS)) {
+            return;
+        }
+        //needs a way to check if item is air or null?
+        CompoundTag itemTag = tools.readItemStack(event.getItem());
+        if (!checkBlacklist(p, itemTag)) {
+            e.setCancelled(true);
         }
     }
 
