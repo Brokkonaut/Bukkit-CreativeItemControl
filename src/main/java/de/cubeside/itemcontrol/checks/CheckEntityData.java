afe3828433c2fdab69cd95258191d2bcd8b038e6
Brokkonaut <hannos17@gmx.de>
2024-09-18 07:03:40 +0200

    allow paintings from creative inventory

--- src/main/java/de/cubeside/itemcontrol/checks/CheckEntityData.java
+++ src/main/java/de/cubeside/itemcontrol/checks/CheckEntityData.java
@@ -3,6 +3,8 @@ package de.cubeside.itemcontrol.checks;
 import de.cubeside.itemcontrol.config.GroupConfig;
 import de.cubeside.itemcontrol.util.ConfigUtil;
 import de.cubeside.nmsutils.nbt.CompoundTag;
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
@@ -11,6 +13,8 @@ public class CheckEntityData implements ComponentCheck {
     private static final NamespacedKey KEY = NamespacedKey.fromString("minecraft:entity_data");
 
     private boolean allow;
+
+    private boolean allowPaintings;
 
     @Override
     public NamespacedKey getComponentKey() {
@@ -21,11 +25,37 @@ public class CheckEntityData implements ComponentCheck {
     public void loadConfig(ConfigurationSection section) {
         ConfigurationSection data = ConfigUtil.getOrCreateSection(section, KEY.asMinimalString());
         allow = ConfigUtil.getOrCreate(data, "allow", false);
+        allowPaintings = ConfigUtil.getOrCreate(data, "allowPaintings", true);
     }
 
     @Override
     public boolean enforce(GroupConfig group, Material material, CompoundTag itemComponentsTag, String key) {
         boolean changed = false;
+        CompoundTag entityData = itemComponentsTag.getCompound(key);
+        if (material == Material.PAINTING && entityData != null) {
+            if (allowPaintings) {
+                String id = entityData.getString("id");
+                if (id == null || !(id.equals("minecraft:painting") || id.equals("painting"))) {
+                    itemComponentsTag.remove(key);
+                    changed = true;
+                } else {
+                    String variant = entityData.getString("variant");
+                    NamespacedKey variantKey = variant == null ? null : NamespacedKey.fromString(variant);
+                    if (variantKey == null || RegistryAccess.registryAccess().getRegistry(RegistryKey.PAINTING_VARIANT).get(variantKey) == null) {
+                        itemComponentsTag.remove(key);
+                        changed = true;
+                    } else {
+                        for (String s : entityData.getAllKeys()) {
+                            if (!s.equals("id") && !s.equals("variant")) {
+                                entityData.remove(s);
+                                changed = true;
+                            }
+                        }
+                    }
+                }
+                return changed;
+            }
+        }
         if (!allow) {
             itemComponentsTag.remove(key);
             changed = true;
